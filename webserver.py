2800da1ba87ddb138547a5d379ea91bd62039220
John Leszczynski <jrleszczynski@gmail.com>
2015-06-01 17:15:45 -0400

    display restaurant names at /restaurants

--- webserver.py
+++ webserver.py
@@ -1,6 +1,15 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
-import database_setup
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
 
 
 class webserverHandler(BaseHTTPRequestHandler):
@@ -13,10 +22,8 @@ class webserverHandler(BaseHTTPRequestHandler):
 
                 output = ""
                 output += "<html><body>"
-                output += "Hello!"
-                output += """<form method='POST' enctype='multipart/form-data' action='/
-                    hello'><h2>What would you like me to say?</h2><input name='message' 
-                    type='text' ><input type='submit' value='Submit'> </form>"""
+                for restaurant in session.query(Restaurant).all():
+                    output += "<p>%s</p>" %restaurant.name
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output

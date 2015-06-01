ec9216a6fde381d5d91bfe66acb4ccb90129f12b
John Leszczynski <jrleszczynski@gmail.com>
2015-06-01 17:15:23 -0400

    add line break after __tablename__s

--- database_setup.py
+++ database_setup.py
@@ -5,18 +5,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
-
+ 
 Base = declarative_base()
 
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
+    
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
     description = Column(String(250))
